ed759b052a9ff7e8ca543116ef015d77c6697bcb
iam-harshit <importantemail545@gmail.com>
2024-07-12 03:27:33 +0530

    Add shimmer effect in display food items list

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -4,17 +4,32 @@ import { currency } from "../../assets/assets";
 import { BACKEND_URL } from "../../utils/Constant";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { ShimmerCategoryItem } from "react-shimmer-effects";
 
 const List = () => {
   const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState({});
 
   const fetchList = async () => {
-    const response = await axios.get(`${BACKEND_URL}/api/food/list`);
-    if (response.data.success) {
+    setLoading(true);
+    try {
+      const response = await axios.get(`${BACKEND_URL}/api/food/list`);
       setList(response.data.data);
-    } else {
-      toast.error("Error");
+      initializeImageLoadingState(response.data.data);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
     }
+    setLoading(false);
+  };
+
+  const initializeImageLoadingState = (items) => {
+    const initialState = items.reduce((acc, item) => {
+      acc[item._id] = false;
+      return acc;
+    }, {});
+    setImagesLoaded(initialState);
   };
 
   const removeFood = async (foodId) => {
@@ -29,9 +44,23 @@ const List = () => {
     }
   };
 
+  const handleImageLoad = (id) => {
+    setImagesLoaded((prev) => ({ ...prev, [id]: true }));
+  };
+
+  const allImagesLoaded = () => {
+    return Object.values(imagesLoaded).every((loaded) => loaded);
+  };
+
   useEffect(() => {
     fetchList();
   }, []);
+
+  useEffect(() => {
+    if (list.length > 0 && allImagesLoaded()) {
+      setLoading(false);
+    }
+  }, [imagesLoaded, list]);
 
   return (
     <div className="list add flex-col">
@@ -44,22 +73,50 @@ const List = () => {
           <b>Price</b>
           <b>Action</b>
         </div>
-        {list.map((item, index) => {
-          return (
-            <div key={index} className="list-table-format">
-              <img src={item.image} alt="" />
-              <p>{item.name}</p>
-              <p>{item.category}</p>
-              <p>
-                {currency}
-                {item.price}
-              </p>
-              <p className="cursor" onClick={() => removeFood(item._id)}>
-                x
-              </p>
-            </div>
-          );
-        })}
+        {loading ? (
+          <div>
+            {list.map((item) => (
+              <ShimmerCategoryItem
+                key={item._id}
+                hasImage
+                imageType="thumbnail"
+                imageWidth={0}
+                imageHeight={0}
+                title
+              />
+            ))}
+          </div>
+        ) : (
+          list.map((item) => {
+            return (
+              <div key={item._id} className="list-table-format">
+                {!imagesLoaded[item._id] && (
+                  <ShimmerCategoryItem
+                    hasImage
+                    imageType="thumbnail"
+                    imageWidth={0}
+                    imageHeight={30}
+                  />
+                )}
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  onLoad={() => handleImageLoad(item._id)}
+                  style={{ display: imagesLoaded[item._id] ? "block" : "none" }}
+                />
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>
+                  {currency}
+                  {item.price}
+                </p>
+                <p className="cursor" onClick={() => removeFood(item._id)}>
+                  x
+                </p>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
